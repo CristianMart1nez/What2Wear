b041aa011b99b77bb0e336340ea7573a0b7d8a63
ssamusr <samusr@gmail.com>
2024-04-22 16:24:40 +0100

    Created login page and modified services user.js

--- what2wear/src/services/user.js
+++ what2wear/src/services/user.js
@@ -4,3 +4,8 @@ export const userRegister = async (body) => {
     const {data} = await api.post('auth/register', body)
     return data
 }
+
+export const userLogin = async () => {
+    const {data} = await api.post('auth/login')
+    return data
+}

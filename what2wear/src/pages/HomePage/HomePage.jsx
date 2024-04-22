03db61b1d495ccf96c6e4a1bce689dee6acea98d
ssamusr <samusr@gmail.com>
2024-04-22 17:00:45 +0100

    Tried fonts on HomePage

--- what2wear/src/pages/HomePage/HomePage.jsx
+++ what2wear/src/pages/HomePage/HomePage.jsx
@@ -2,6 +2,12 @@ import './HomePage.css'
 
 export const HomePage = () => {
   return (
-    <h1>HomePage</h1>
+    <section className='prueba'>
+      <h1>HomePage</h1>
+      <h2>Personal Stylist</h2>
+      <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, pariatur voluptatem? Alias temporibus id iusto natus dolorem! Qui sapiente veniam voluptatum libero expedita corrupti amet velit. Temporibus quibusdam vero doloribus?</h3>
+
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint tempora inventore eaque exercitationem deserunt rem, cum molestias minima ducimus alias, ad sapiente sunt facilis a ab voluptates culpa debitis accusamus?</p>
+    </section>
   )
 }

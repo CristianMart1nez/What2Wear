ed5b368aa23227988086fee998aa33adb1cc5ac3
ssamusr <samusr@gmail.com>
2024-04-24 09:27:33 +0100

    HomePage sytles

--- what2wear/src/pages/HomePage/HomePage.jsx
+++ what2wear/src/pages/HomePage/HomePage.jsx
@@ -3,26 +3,28 @@ import './HomePage.css'
 export const HomePage = () => {
   return (
     <section className='home-container'>
+      <div className='images-container'>
+        <h2>Personal Stylist</h2>
 
-      <h2>Personal Stylist</h2>
+        <article className='img-container'>
+          <img src="./src/assets/images/capa.jpg" alt="" />
+        </article>
 
-      <article className='img-container'>
-        <img src="./src/assets/images/capa.jpg" alt="" />
-      </article>
+        <article className='img-container'>
+          <img src="./src/assets/images/capa.jpg" alt="" />
+        </article>
 
-      <article className='img-container'>
-        <img src="./src/assets/images/capa.jpg" alt="" />
-      </article>
+        <article className='img-container'>
+          <img src="./src/assets/images/capa.jpg" alt="" />
+        </article>
 
-      <article className='img-container'>
-        <img src="./src/assets/images/capa.jpg" alt="" />
-      </article>
+        <article className='img-container'>
+          <img src="./src/assets/images/capa.jpg" alt="" />
+        </article>
 
-      <article className='img-container'>
-        <img src="./src/assets/images/capa.jpg" alt="" />
-      </article>
+        <button className='btn btn-home'>Create your style now</button>
 
-      <button className='btn btn-home'>Create your style now</button>
+      </div>
 
     </section>
   )

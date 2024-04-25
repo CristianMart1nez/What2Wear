1a309f8ad008885e4c93ea39a361478ac06573b0
Samuel <samusr@gmail.com>
2024-04-25 20:56:05 +0100

    Added slider on homepage

--- what2wear/src/pages/HomePage/HomePage.jsx
+++ what2wear/src/pages/HomePage/HomePage.jsx
@@ -6,8 +6,49 @@ export const HomePage = () => {
       <div className='images-container'>
         <h2>Personal Stylist</h2>
 
-        <article className='img-container'>
-          <img src="./src/assets/images/cover_img/cover1.jpg" alt="" />
+        <article className='img-container slider'>
+
+          <div className="slide-track">
+
+            <div className="slide">
+              <img src="./src/assets/images/Fotos-rotativas/Foto-rotativa-1.png" alt="" />
+            </div>
+            <div className="slide">
+              <img src="./src/assets/images/Fotos-rotativas/Foto-rotativa-2.png" alt="" />
+            </div>
+            <div className="slide">
+              <img src="./src/assets/images/Fotos-rotativas/Foto-rotativa-3.png" alt="" />
+            </div>
+            <div className="slide">
+              <img src="./src/assets/images/Fotos-rotativas/Foto-rotativa-1.png" alt="" />
+            </div>
+            <div className="slide">
+              <img src="./src/assets/images/Fotos-rotativas/Foto-rotativa-2.png" alt="" />
+            </div>
+            <div className="slide">
+              <img src="./src/assets/images/Fotos-rotativas/Foto-rotativa-3.png" alt="" />
+            </div>
+            <div className="slide">
+              <img src="./src/assets/images/Fotos-rotativas/Foto-rotativa-1.png" alt="" />
+            </div>
+            <div className="slide">
+              <img src="./src/assets/images/Fotos-rotativas/Foto-rotativa-2.png" alt="" />
+            </div>
+            <div className="slide">
+              <img src="./src/assets/images/Fotos-rotativas/Foto-rotativa-3.png" alt="" />
+            </div>
+            <div className="slide">
+              <img src="./src/assets/images/Fotos-rotativas/Foto-rotativa-1.png" alt="" />
+            </div>
+            <div className="slide">
+              <img src="./src/assets/images/Fotos-rotativas/Foto-rotativa-2.png" alt="" />
+            </div>
+            <div className="slide">
+              <img src="./src/assets/images/Fotos-rotativas/Foto-rotativa-3.png" alt="" />
+            </div>
+
+          </div>
+
         </article>
 
         <article className='img-container'>
@@ -27,6 +68,6 @@ export const HomePage = () => {
       </div>
 
     </section>
-    
+
   )
 }

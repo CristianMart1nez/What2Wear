eb52f85b4513ee9eb1f8305caf659beb98d35b6a
Samuel S. Rodriguez <samusr@gmail.com>
2024-04-25 21:03:18 +0100

    Added scroll on trending page

--- what2wear/src/pages/TrendingPage/TrendingPage.jsx
+++ what2wear/src/pages/TrendingPage/TrendingPage.jsx
@@ -1,83 +1,33 @@
-import { useEffect } from 'react'
-import './TrendingPage.css'
+import { useEffect } from "react";
+import "./TrendingPage.css";
+
+const images = [
+  './src/assets/images/trending/trending-2.jpg',
+  './src/assets/images/trending/trending-4.jpg',
+  './src/assets/images/trending/trending-6.jpg',
+  './src/assets/images/trending/trending-8.jpg',
+  // Agrega más imágenes según sea necesario
+];
 
 export const TrendingPage = () => {
-
   useEffect(() => {
-    document.title = "What2Wear | Trending"
-  }, [])
+    document.title = "What2Wear | Trending";
+  }, []);
 
   return (
-<>
-<section className='Pagina-trending'> 
-  
- <article className="img-card1">
-   <a href="/">
-    <img src="./src/assets/images/trending/00881332800-a1.jpg"  alt="" />
-    </a>  
-  </article>
-  </section>
-
- {/*  <section className='Pagina-2-fotos'>     
- <article className="img-card2">
-   <a href="/">
-    <img src="./src/assets/images/trending/02895823711-p.jpg" alt="" />
-    </a>  
-  </article>
-  
-  <article className="img-card2">
-  <img src="./src/assets/images/trending/08741062711-015-p.jpg"alt="" />
-</article>
-</section>
-  
-
- <section className='Pagina-1-foto'>     
- <article className="img-card1">
-   <a href="/">
-    <img src="./src/assets/images/trending/02879482808-p.jpg" alt="" />
-    </a>  
-  </article>
-  </section>
-
-  <section className='Pagina-2-fotos'>     
- <article className="img-card2">
-   <a href="/">
-    <img src="./src/assets/images/trending/02906204330-p.jpg" alt="" />
-    </a>  
-  </article>
-  
-  <article className="img-card2">
-  <img src="./src/assets/images/trending/03084108251-p.jpg" alt="" />
-</article>
-</section>
-
-<section className='Pagina-1-foto'>     
- <article className="img-card1">
-   <a href="/">
-    <img src="./src/assets/images/trending/02565098330-p.jpg"alt="" />
-    </a>  
-  </article>
-  </section>
-
-<section className='Pagina-2-fotos'>     
- <article className="img-card2">
-   <a href="/">
-    <img src="./src/assets/images/outfits_sug_img/img_woman/00085326520-p.jpg" alt="" />
-    </a>  
-  </article>
-  
-  <article className="img-card2">
-  <img src="./src/assets/images/trending/05427436718-p.jpg" alt="" />
-</article>
-</section>
-
- */}
-
-
- 
-
-
-
- </>
-  )}
- 
+    <div className="container-scroll-animation">
+      <section className="one">
+        <h1>First Image</h1>
+      </section>
+      <section className="two">
+        <h1>Second Image</h1>
+      </section>
+      <section className="three">
+        <h1>Three Image</h1>
+      </section>
+      <section className="four">
+        <h1>Four Image</h1>
+      </section>
+    </div>
+  );
+}
